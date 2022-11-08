bc31c479aca40e77c1b72b50294e2cf285597544
Ernesto <ernestocabanasdev@gmail.com>
2022-11-08 17:09:33 -0600

    Initial structure

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+/* Open */
+function openNav() {
+    document.getElementById("menu").style.height = "100%";
+}
+  
+/* Close */
+function closeNav() {
+    document.getElementById("menu").style.height = "0%";
+} 
