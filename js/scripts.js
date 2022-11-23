ae2a1784e236d617f9975c37d1d2809d33b15f97
Ernesto <ernestocabanasdev@gmail.com>
2022-11-22 19:10:01 -0600

    Changes status:
    -About page ready. \n-TODO: Contact, Experience and Policy.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,32 @@
+const indexContent = document.getElementById("about-me");
+const contentFrame = document.getElementById("content");
+const contentShow = document.getElementById("content-show");
+const contactTitle = "It would be a pleasure to meet you.";
+const experienceTitle = "The harder I work, the luckier I get.";
+const indexTitle = "Programming is my passion, Not my job.";
+
 /* Open */
 function openNav() {
-    document.getElementById("menu").style.height = "100%";
+  document.getElementById("menu").style.height = "100%";
 }
-  
+
 /* Close */
 function closeNav() {
-    document.getElementById("menu").style.height = "0%";
-} 
+  document.getElementById("menu").style.height = "0%";
+}
+
+function showPage(pageName) {
+  if (pageName != "about-me") {
+    indexContent.style.display = "none";
+    contentShow.style.display = "block";
+    contentFrame.src = pageName + ".html";
+  } else {
+    indexContent.style.display = "block";
+    contentShow.style.display = "none";
+  }
+}
+
+function setContent() {
+  contentShow.innerHTML = contentFrame.contentDocument.body.innerHTML;
+  
+}
